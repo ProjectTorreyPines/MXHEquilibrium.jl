34d4d4294e77f3a4677f9caa9c6c7f82944b109d
Orso Meneghini <orso82@gmail.com>
2023-08-10 09:05:33 -0700

    formatting

--- src/transp_io.jl
+++ src/transp_io.jl
@@ -1,13 +1,13 @@
 using NetCDF
 
 function transp_potential!(M, transp_file, time)
-    tt = Float64.(ncread(transp_file,"TIME"))
-    ind = argmin(abs.(tt .- 1e-3*time))
-    psi = Float64.(ncread(transp_file,"PLFLX")[:,ind])
-    epot = Float64.(ncread(transp_file,"EPOTNC")[:,ind])
-    phi_itp = linear_interpolation(psi,epot,extrapolation_bc=Flat())
+    tt = Float64.(ncread(transp_file, "TIME"))
+    ind = argmin(abs.(tt .- 1e-3 * time))
+    psi = Float64.(ncread(transp_file, "PLFLX")[:, ind])
+    epot = Float64.(ncread(transp_file, "EPOTNC")[:, ind])
+    phi_itp = linear_interpolation(psi, epot, extrapolation_bc=Flat())
     phi = phi_itp.(M.psi)
-    phi_itp = cubic_spline_interpolation(M.psi,phi,extrapolation_bc=Flat())
+    phi_itp = cubic_spline_interpolation(M.psi, phi, extrapolation_bc=Flat())
     M.phi = phi_itp
     nothing
 end
